ec2d59d28e997dc960b9ae95f97cf88fab6614dc
Edward Drozdov <ekdrozdov@gmail.com>
2021-10-03 01:23:17 +0700

    WIP add targeting ui

--- Assets/Scripts/Caster.cs
+++ Assets/Scripts/Caster.cs
@@ -6,6 +6,23 @@ public class Caster : MonoBehaviour
 
   void Update()
   {
-
+    if (Input.GetMouseButtonDown(0))
+    {
+      RaycastHit hitInfo = new RaycastHit();
+      bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+      if (hit)
+      {
+        if (hitInfo.transform.gameObject.tag == "Selectable")
+        {
+          // _ss.Target(hitInfo.transform.gameObject.GetComponent<SimplePropertyContainer>());
+          // TargetNameLabel.text = hitInfo.transform.gameObject.GetComponent<SimplePropertyContainer>().name;
+        }
+      }
+    }
+    if (Input.GetKey(KeyCode.Escape))
+    {
+      // _ss.ClearTarget();
+      // TargetNameLabel.text = "No unit selected";
+    }
   }
 }
